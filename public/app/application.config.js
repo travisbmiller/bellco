64a37f89d5bbe9f5962f56b8784e39244e5f5399
Travis Miller <travisbmiller@outlook.com>
2015-05-05 15:48:54 -1000

    updated states for application steps

--- public/app/application.config.js
+++ public/app/application.config.js
@@ -11,19 +11,54 @@
       
         $stateProvider
             .state('apply', {
-              url: "/",
-              templateUrl: "./app/submit-application/appl.html",
+              abstract: true,
+              templateUrl: "./app/submit-application/temp.html",
               controllerAs: 'vm',
               controller: 'submitController'
-            
             })
+            .state('apply.step1', {
+              url: "/apply",
+              templateUrl: "./app/submit-application/step1.html",
+            })
+            .state('apply.step2', {
+              url: "/apply1",
+              templateUrl: "./app/submit-application/step2.html",
+            })
+            .state('apply.step3', {
+              url: "/apply2",
+              templateUrl: "./app/submit-application/step3.html",
+            })
+            .state('apply.step4', {
+              url: "/apply3",
+              templateUrl: "./app/submit-application/step4.html",
+            })
+            .state('apply.step5', {
+              url: "/apply4",
+              templateUrl: "./app/submit-application/step5.html",
+            })
+
+
             .state('state2', {
               url: "/test",
               templateUrl: "./app/submit-application/test.html",
               controllerAs: 'vm',
               controller: 'submitController'
+            })
+            .state('state3', {
+              url: "/dash",
+              templateUrl: "./app/dashboard/dash.html",
+              controllerAs: 'vm',
+              controller: 'Dashboard'
             
             })
+            .state('state4', {
+              url: "/select",
+              templateUrl: "./app/submit-application/date.html",
+              controllerAs: 'vm',
+              controller: 'submitController'
+            
+            })
+
         }
 
 })();
